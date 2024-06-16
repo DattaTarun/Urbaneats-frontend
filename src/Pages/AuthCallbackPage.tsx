cd6a1758a1bd7b82f93a9affb95805f227486322
DattaTarun <97876226+DattaTarun@users.noreply.github.com>
2024-06-16 13:18:40 +0530

    Update AuthCallbackPage.tsx

--- src/Pages/AuthCallbackPage.tsx
+++ src/Pages/AuthCallbackPage.tsx
@@ -1,7 +1,7 @@
 import { useCreateMyUser } from '@/Api/MyUserApi';
 import { useAuth0 } from '@auth0/auth0-react'
-import React, { useEffect, useRef } from 'react'
-import { Navigate, useNavigate } from 'react-router-dom';
+import { useEffect, useRef } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 function AuthCallbackPage() {
     const navigate=useNavigate();
