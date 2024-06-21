fb019e637318cecf462be95a2bd02fd844a4d574
Tarun Datta <tarundatta@Taruns-MacBook-Air.local>
2024-06-21 18:04:50 +0530

    added restaurant sort

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -5,12 +5,14 @@ import AuthCallbackPage from "./Pages/AuthCallbackPage"
 import UserProfilePage from "./Pages/UserProfilePage"
 import ProtectedRoute from "./auth/ProtectedRoute"
 import ManageRestaurantPage from "./Pages/ManageRestaurantPage"
+import SearchPage from "./Pages/SearchPage"
 
 function AppRoutes() {
   return (
     <Routes>
         <Route path="/" element={<Layout showHero><Homepage /></Layout>} />
         <Route path="/auth-callback" element={<AuthCallbackPage />} />
+        <Route path="/search/:city" element={<Layout showHero={false}><SearchPage/></Layout>} />
         <Route element={<ProtectedRoute />}>
           <Route path="/user-profile" element={<Layout><UserProfilePage /></Layout>} />
           <Route path="/manage-restaurant" element={<Layout><ManageRestaurantPage /></Layout>} />
