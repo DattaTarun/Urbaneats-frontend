15e5e72664de9f86409ea24701fd7c73916e8a4b
Tarun Datta <tarundatta@Taruns-MacBook-Air.local>
2024-06-23 17:15:21 +0530

    finall

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -7,6 +7,7 @@ import ProtectedRoute from "./auth/ProtectedRoute"
 import ManageRestaurantPage from "./Pages/ManageRestaurantPage"
 import SearchPage from "./Pages/SearchPage"
 import DetailPage from "./Pages/DetailPage"
+import OrderStatusPage from "./Pages/OrderStatusPage"
 
 function AppRoutes() {
   return (
@@ -16,6 +17,7 @@ function AppRoutes() {
         <Route path="/search/:city" element={<Layout showHero={false}><SearchPage/></Layout>} />
         <Route path="/detail/:restaurantId" element={<Layout showHero={false}><DetailPage/></Layout>} />
         <Route element={<ProtectedRoute />}>
+          <Route path="/order-status" element={<Layout><OrderStatusPage /></Layout>} />
           <Route path="/user-profile" element={<Layout><UserProfilePage /></Layout>} />
           <Route path="/manage-restaurant" element={<Layout><ManageRestaurantPage /></Layout>} />
         </Route>
